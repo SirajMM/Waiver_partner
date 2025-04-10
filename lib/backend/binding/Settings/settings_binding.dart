b737676d54390a97c9ad439fa539e473b172ef1a
Sreenivas Shenoy.V <119277529+sreenivas20@users.noreply.github.com>
2025-04-10 23:42:25 +0530

    fleet issues 2

--- lib/backend/binding/Settings/settings_binding.dart
+++ lib/backend/binding/Settings/settings_binding.dart
@@ -1,9 +1,13 @@
 import 'package:get/get.dart';
 import 'package:waiver_driver/controller/setting/setting_controller.dart';
 
+import '../../api/api_services/api_services.dart';
+import '../../api/api_services/urls.dart';
+
 class SettingScreenBinding extends Bindings {
   @override
   void dependencies() {
+    Get.put(ApiServices(appBaseUrl: AppUrls.base));
     Get.put(SettingController(parser: Get.find()));
   }
 }
